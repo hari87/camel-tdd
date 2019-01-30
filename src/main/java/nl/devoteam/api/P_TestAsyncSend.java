1ee152e6882a17f108b6286f905f8ac6c2609312
hari prasad <hari.stpeters@gmail.com>
2019-01-30 15:39:24 +0100

    Explaining AdviseWith with inline comments

---
+++ src/main/java/nl/devoteam/api/P_TestAsyncSend.java
@@ -0,0 +1,20 @@
+package nl.devoteam.api;
+
+import java.util.concurrent.CompletableFuture;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class P_TestAsyncSend implements Processor {
+
+    @Autowired
+    ProducerTemplate template;
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        CompletableFuture<Object> myobject = template.asyncSendBody("direct:retrieveTroubleTicket", "hello");
+
+
+    }
+}
